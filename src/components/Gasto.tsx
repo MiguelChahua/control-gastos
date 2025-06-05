df0501c15fb5056a284f4bf6f371504214c66c72
MiguelDev <miguel.ang.cv95@gmail.com>
2025-06-04 23:54:05 -0500

    Formulario agregado sin btn de retorno

--- src/components/Gasto.tsx
+++ src/components/Gasto.tsx
@@ -1,9 +1,9 @@
-type GastoType = {
+export type GastoType = {
   nombre: string;
-  valor: number;
+  monto: number;
 };
 
-function Gasto({ nombre, valor }: GastoType) {
+function Gasto({ nombre, monto }: GastoType) {
   return (
     <div className="d-flex justify-content-between align-items-center py-3 px-2 bg-light rounded border">
       <div className="fw-semibold text-dark">{nombre}</div>
@@ -23,7 +23,7 @@ function Gasto({ nombre, valor }: GastoType) {
             d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
           />
         </svg>
-        <span className="fw-bold">{valor}</span>
+        <span className="fw-bold">{monto}</span>
       </div>
     </div>
   );
--- src/components/ListadoGastos.tsx
+++ src/components/ListadoGastos.tsx
@@ -1,28 +1,25 @@
+import { Link } from 'react-router-dom'
 import Gasto from "./Gasto"
+import type { GastoType } from './Gasto';
 
-let listaGastos = [
-    {nombre: 'Alquiler', monto: 500},
-    {nombre: 'Comida', monto: 300},
-    {nombre: 'Luz', monto: 50},
-    {nombre: 'Agua', monto: 50},
-    {nombre: 'Internet', monto: 90},
-    {nombre: 'Transporte', monto: 150}
-]
+export type ListProps = {
+  gastos: GastoType[]
+}
 
-function ListadoGastos() {
+const ListadoGastos = ( {gastos} : ListProps) => {
   return (
     <div className="flex">
       <h2 className="titulo-gastos text-center">Gastos Mensuales</h2>
       <div className="bg-white rounded-3 shadow-lg p-4" style={{width: '300px'}}>
         
         <div className="d-flex flex-column gap-3">
-          {listaGastos.map((g, i) => (
-            <Gasto nombre={g.nombre} valor={g.monto} key={i} />
+          {gastos.map((g, i) => (
+            <Gasto nombre={g.nombre} monto={g.monto} key={i} />
           ))}
         </div>
-        <div className="d-flex justify-content-center mt-4">
-          <button className="btn btn-primary">Agregar gasto</button>
-        </div>
+        <Link to="/nuevo" className="d-flex justify-content-center mt-4">
+          <button className="btn btn-primary">➕ Nuevo gasto</button>
+        </Link>
       </div>
     </div>
   )
